24201fac82608ffd725aff6e2fc71bf6a5718ce4
Felipe Carvalho <P5180242@brasilprev.com.br>
2020-10-03 20:28:42 -0300

    test3

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -47,6 +47,9 @@ class Home extends React.Component {
       headers: {
         "Content-Type": "application/json",
         'Access-Control-Allow-Origin': '*'
+        "Access-Control-Allow-Credentials", "true",
+        "Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
       }
     })
     .then((response) => {
